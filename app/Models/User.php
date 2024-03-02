87e9086718ea7e3ebe34f4c897f2dfeb62012b68
ddev01 <71857399+ddev01@users.noreply.github.com>
2024-03-02 01:03:12 +0100

    Fix images

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,47 +3,47 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-	use HasApiTokens, HasFactory, HasUuids, Notifiable;
+    use HasApiTokens, HasFactory, HasUuids, Notifiable;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $fillable = ['name', 'email', 'password', 'avatar'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['name', 'email', 'password', 'avatar'];
 
-	/**
-	 * The attributes that should be hidden for serialization.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $hidden = ['password', 'remember_token'];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = ['password', 'remember_token'];
 
-	/**
-	 * The attributes that should be cast.
-	 *
-	 * @var array<string, string>
-	 */
-	protected $casts = [
-		'email_verified_at' => 'datetime',
-		'password' => 'hashed',
-	];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
-	public function getAvatar()
-	{
-		if ($this->avatar) {
-			return Storage::disk('public')->url("{$this->avatar}");
-		} else {
-			return Storage::disk('public')->url('avatars/default-avatar.png');
-		}
-	}
+    public function getAvatar()
+    {
+        if ($this->avatar) {
+            return Storage::disk('public')->url("{$this->avatar}");
+        } else {
+            return asset('images/default-avatar.png');
+        }
+    }
 }
